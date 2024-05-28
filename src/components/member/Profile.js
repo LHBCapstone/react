707cf0ddb04ccd7a3711e5dcf2c7148c2a0a561e
dldks3467 <dldks3467@gmail.com>
2024-05-28 16:45:54 +0900

    update profile

--- src/components/member/Profile.js
+++ src/components/member/Profile.js
@@ -1,15 +1,14 @@
 import { useEffect, useState } from "react";
 import { Form, Button } from "react-bootstrap";
+import { useCookies } from "react-cookie";
 
 const Profile = () => {
-  const [email, setEmail] = useState("");
   const [name, setName] = useState("");
   const [password, setPassword] = useState("");
   const [checkPassword, setCheckPassword] = useState("");
+  const [cookies, setCookie, removeCookie] = useCookies(["user"]);
+  const email = cookies.user;
 
-  const chEmail = (event) => {
-    setEmail(event.target.value);
-  };
   const chName = (event) => {
     setName(event.target.value);
   };
@@ -19,9 +18,39 @@ const Profile = () => {
   const chCheckPassword = (event) => {
     setCheckPassword(event.target.value);
   };
+  //이벤트 호출을 하지 않아도 자동으로 가장먼저 호출됨
   useEffect(() => {
-    fetch();
+    const cookieEmail = {
+      email: email,
+    };
+    const data = {
+      name: name,
+      password: password,
+      checkPassword: checkPassword,
+    };
+    fetch("http://localhost:8080/user/profile", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json; charset=utf-8",
+      },
+      body: JSON.stringify(cookieEmail),
+    })
+      .then((res) => res)
+      .then(
+        (res) => {
+          if (res.status === 200) {
+            alert(res.name);
+            setName(res.name);
+          } else {
+            alert("로그인 실패");
+          }
+        },
+        (err) => {
+          alert(err);
+        }
+      );
   });
+
   const changePassword = () => {};
   const changeName = () => {};
   return (
@@ -29,7 +58,7 @@ const Profile = () => {
       <div>
         <Form>
           <Form.Label>Email</Form.Label>
-          <Form.Control value={email} disabled onChange={chEmail} />
+          <Form.Control value={email} disabled />
           <Form.Label>이름</Form.Label>
           <Form.Control value={name} onChange={chName} />
           <Button onClick={changeName}>이름 변경</Button>
